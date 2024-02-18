3b68e68efc6b578f08ed52777b88e1205077464a
Florian Ellis <florian.ellis@qonto.com>
2024-02-18 15:40:21 +0100

    Improve layout of pin details screen

--- src/screens/PinDetailsScreen.styles.ts
+++ src/screens/PinDetailsScreen.styles.ts
@@ -5,24 +5,31 @@ export const SIDE_PADDING = 6;
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
-    paddingLeft: SIDE_PADDING,
-    paddingRight: SIDE_PADDING,
-    backgroundColor: "black",
     flex: 1,
-  },
-  content: {
     position: "relative",
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    backgroundColor: Colors.backgroundBase,
-    flex: 1,
   },
   backButton: {
     position: "absolute",
     zIndex: 10,
-    top: 20,
-    left: 20,
+    top: 80,
+    left: 26,
+  },
+  backgroundAndContent: {
+    flex: 1,
+    paddingTop: 60,
+    paddingLeft: SIDE_PADDING,
+    paddingRight: SIDE_PADDING,
+    backgroundColor: "black",
+  },
+  content: {
+    minHeight: "100%", // For some reason setting 'flex: 1' here doesn't
+    // allow to cover the whole bottom of the screen with a white
+    // background, in the cases where the pin's content is less than the
+    // screen's height.
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    backgroundColor: Colors.backgroundBase,
+    paddingBottom: 80,
   },
   backButtonIcon: {
     color: "white",
@@ -34,16 +41,33 @@ const styles = StyleSheet.create({
   authorData: {
     flexDirection: "row",
     alignItems: "center",
+    marginTop: 20,
   },
   authorName: {
     fontSize: Fonts.fontSize300,
     fontWeight: Fonts.fontWeightSemiBold,
+    paddingRight: 10,
   },
   authorProfilePictureImage: {
     height: 40,
     width: 40,
     borderRadius: 20,
-    margin: 20,
+    marginLeft: 20,
+    marginRight: 16,
+  },
+  pinTitle: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: Fonts.fontSize300,
+    fontWeight: Fonts.fontWeightSemiBold,
+    paddingRight: 10,
+  },
+  pinDescription: {
+    marginTop: 6,
+    marginLeft: 20,
+    paddingRight: 10,
+    fontSize: Fonts.fontSize200,
   },
 });
 
