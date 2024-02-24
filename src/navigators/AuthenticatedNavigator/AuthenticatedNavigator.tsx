3f375c59db238dd8f0a4d2c45f6ab2ecffd56bf5
Florian Ellis <florian.ellis@qonto.com>
2024-02-24 15:13:24 +0100

    Refactor <AuthenticatedNavigator /> to insert new navigator

--- src/navigators/AuthenticatedNavigator/AuthenticatedNavigator.tsx
+++ src/navigators/AuthenticatedNavigator/AuthenticatedNavigator.tsx
@@ -1,89 +1,20 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { RouteProp } from "@react-navigation/native";
-import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
-
-import HomeNavigator from "../HomeNavigator/HomeNavigator";
-import SearchNavigator from "../SearchNavigator/SearchNavigator";
-
-import ProfileScreen from "@/src/navigators/AuthenticatedNavigator/ProfileScreen";
-import { useState } from "react";
-import CreateSelectModal from "@/src/components/CreateSelectModal/CreateSelectModal";
-
-type AuthenticatedNavigatorParamList = {
-  Home: undefined;
-  Search: undefined;
-  Create: undefined;
-  Profile: undefined;
-};
-
-const TAB_BAR_ICON_NAMES: Record<string, string> = {
-  Home: "home",
-  Search: "search",
-  Create: "plus",
-  Profile: "user",
-};
+import { createStackNavigator } from "@react-navigation/stack";
+import AuthenticatedMainNavigator from "../AuthenticatedMainNavigator/AuthenticatedMainNavigator";
 
 const AuthenticatedNavigator = () => {
-  const [isCreateSelectModalVisible, setIsCreateSelectModalVisible] =
-    useState(false);
-
-  const TabNavigator =
-    createBottomTabNavigator<AuthenticatedNavigatorParamList>();
-
-  const screenOptions = ({
-    route,
-  }: {
-    route: RouteProp<AuthenticatedNavigatorParamList>;
-  }) => {
-    const tabBarIcon = ({ color }: { color: string }) => (
-      <FontAwesome5Icon
-        name={TAB_BAR_ICON_NAMES[route.name]}
-        size={24}
-        color={color}
-      />
-    );
-
-    return {
-      tabBarIcon,
-      tabBarShowLabel: false,
-      headerShown: false,
-      tabBarActiveTintColor: "black",
-      tabBarInactiveTintColor: "gray",
-    };
-  };
-
-  const createTabPressListener = (event: any) => {
-    event.preventDefault(); // prevent regular navigation to "Create" screen (which renders nothing)
-
-    setIsCreateSelectModalVisible(true);
-  };
-
-  const handlePressCreatePin = () => {};
-
-  const handleCloseCreateSelectModal = () => {
-    setIsCreateSelectModalVisible(false);
-  };
+  const StackNavigator = createStackNavigator();
 
   return (
-    <>
-      <TabNavigator.Navigator screenOptions={screenOptions}>
-        <TabNavigator.Screen name="Home" component={HomeNavigator} />
-        <TabNavigator.Screen name="Search" component={SearchNavigator} />
-        <TabNavigator.Screen
-          name="Create"
-          listeners={{ tabPress: createTabPressListener }}
-        >
-          {() => null}
-        </TabNavigator.Screen>
-        <TabNavigator.Screen name="Profile" component={ProfileScreen} />
-      </TabNavigator.Navigator>
-      {isCreateSelectModalVisible && (
-        <CreateSelectModal
-          handlePressCreatePin={handlePressCreatePin}
-          handleClose={handleCloseCreateSelectModal}
-        />
-      )}
-    </>
+    <StackNavigator.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <StackNavigator.Screen
+        name="Main"
+        component={AuthenticatedMainNavigator}
+      />
+    </StackNavigator.Navigator>
   );
 };
 
