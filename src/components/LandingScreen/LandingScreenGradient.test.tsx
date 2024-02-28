d1770c3c84fc90fe9f34d0fc4f84135baccaeb51
Florian Ellis <florian.ellis@qonto.com>
2024-02-28 05:45:32 +0100

    Add various unit tests

---
+++ src/components/LandingScreen/LandingScreenGradient.test.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react-native";
+
+import LandingScreenGradient from "./LandingScreenGradient";
+
+it("renders", () => {
+  render(<LandingScreenGradient />);
+});
